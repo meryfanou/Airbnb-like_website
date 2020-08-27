8138ad3e57c54f80797633cd8f4e6b1798a261f9
dfwteinos <dimfwteinos@gmail.com>
2020-08-27 17:44:54 +0000

    add show page for host's apartements

--- models/apartement.js
+++ models/apartement.js
@@ -41,9 +41,11 @@ var apartementSchema = mongoose.Schema({
 	availability_to: String,			// dates availble to rent
 	capacity: Number,					// maximum number of persons
 	host: {
+	
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "User"
 	},
+									   
 	reservations: [
 		{
 			tenant: {
