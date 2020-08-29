2caab4ad327dea2a3cc618a0eb986f1f4e186aff
meryfanou <mery.fanou@gmail.com>
2020-08-29 14:10:12 +0000

    add profile picture

--- routes/users.js
+++ routes/users.js
@@ -62,7 +62,9 @@ router.get("/:id/edit",function(req,res){
 
 	
 router.put("/:id", upload.single("image"), function(req,res){
+	// If the user uploaded a new profile picture
 	if(req.file){
+		// New profile picture will be in 'result'
 		cloudinary.uploader.upload(req.file.path, function(result){
 			// We want to store the image's secure_url (https://)
 			req.body.user.picture = result.secure_url;
